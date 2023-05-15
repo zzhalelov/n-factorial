a2b57c19f5561349f6e0dc353a82528581991659
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-05-15 16:57:20 +0600

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число");
+        int n = scan.nextInt();
         Factorial f = new Factorial();
-        System.out.println(f.calculateFactorial(4));
+        System.out.println("Факториал числа "+n+" равен "+f.calculateFactorial(n));
     }
 }
