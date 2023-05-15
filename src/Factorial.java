e42851f334b568ba711f336ab7710ccc79603d78
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-05-15 16:41:01 +0600

    1st commit

---
+++ src/Factorial.java
@@ -0,0 +1,9 @@
+public class Factorial {
+    public int calculateFactorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        System.out.println(f.calculateFactorial(4));
+    }
+}
